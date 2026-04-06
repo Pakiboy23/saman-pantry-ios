5c0d68519efd0b52d4e53c2cd50aec5edcd96543
pakiboy23 <s.haarisshariff@gmail.com>
2026-04-06 13:05:30 -0400

    Initial Commit

---
+++ Saman/SamanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamanApp.swift
+//  Saman
+//
+//  Created by Syed Haaris Shariff on 4/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SamanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SamanTests/SamanTests.swift
@@ -0,0 +1,18 @@
+//
+//  SamanTests.swift
+//  SamanTests
+//
+//  Created by Syed Haaris Shariff on 4/6/26.
+//
+
+import Testing
+
+struct SamanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
